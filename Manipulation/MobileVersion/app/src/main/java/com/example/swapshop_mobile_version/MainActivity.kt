43ee027420d021cc95f6cdafcd1ad6b8eb003c12
hakimBarbaria <hakimBarbaria589@gmail.com>
2023-10-18 18:00:07 -0700

    add a manipulation folder with mobile version

---
+++ Manipulation/MobileVersion/app/src/main/java/com/example/swapshop_mobile_version/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.swapshop_mobile_version
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
