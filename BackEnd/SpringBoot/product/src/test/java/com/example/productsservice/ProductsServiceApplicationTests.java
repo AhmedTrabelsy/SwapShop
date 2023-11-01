6aa0942e6e55a69c8342c358e8bbcfa7b49fdee8
Haythem Dridi <haithemdridiweb@gmail.com>
2023-11-01 21:09:46 +0100

    Copy bayrem project

---
+++ BackEnd/SpringBoot/product/src/test/java/com/example/productsservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
