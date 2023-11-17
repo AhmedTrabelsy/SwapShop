de00b8bbf85e240488884b96017db1138bfaafda
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-17 06:04:03 +0100

    create the ui component

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/libs/ui/src/lib/ui/ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swap-shop-ui',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui.component.html',
+  styleUrls: ['./ui.component.css'],
+})
+export class UiComponent {}
