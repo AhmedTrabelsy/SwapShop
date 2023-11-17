076e48b78c8c3148d0836f6c3a2e7b60864d1bbf
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-17 06:44:44 +0100

    add product-list component

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swap-shop-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent {}
