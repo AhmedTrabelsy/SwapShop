4ea4d7f0189be53582e63bcf2393e5d9154294b9
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-17 07:05:13 +0100

    add product library

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/libs/products/src/lib/products/products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swap-shop-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent {}
