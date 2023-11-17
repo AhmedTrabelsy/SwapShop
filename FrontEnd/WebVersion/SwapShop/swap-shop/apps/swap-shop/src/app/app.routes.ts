fbd88d7a68d6a16398cd08d2c00863154fbb6777
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-17 06:44:57 +0100

    add home-page router outlet

--- FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/app.routes.ts
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Route } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [{
+  path: '',
+  component: HomePageComponent
+}];
