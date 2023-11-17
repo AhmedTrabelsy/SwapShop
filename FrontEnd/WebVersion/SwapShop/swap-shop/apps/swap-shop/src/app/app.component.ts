9f97dbefb22e47260cfcf7ef2cc84df33258d192
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-17 10:23:10 +0100

    fix product routes extra '/'

--- FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/app.component.ts
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BannerComponent, UiComponent } from '@swap-shop/ui';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, BannerComponent, UiComponent],
+  imports: [RouterModule, BannerComponent, UiComponent, ProductListComponent],
   selector: 'swap-shop-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
--- FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/app.routes.ts
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/app.routes.ts
@@ -2,11 +2,7 @@ import { Route } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 
-export const appRoutes: Route[] = [{
-  path: '',
-  component: HomePageComponent
-},
-{
-  path: '/products',
-  component: ProductListComponent
-}];
+export const appRoutes: Route[] = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent }
+];
