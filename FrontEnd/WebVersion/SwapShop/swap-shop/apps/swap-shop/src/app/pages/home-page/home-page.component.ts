308cdf84291db27dfd6a93bc01ba78c7510d08c7
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-17 06:44:31 +0100

    add home-page component

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop/src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swap-shop-home-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+})
+export class HomePageComponent {}
