7960144316f177b0b843a78f6316adaf17749d3b
Haythem Dridi <haithemdridiweb@gmail.com>
2023-11-18 16:13:32 +0100

    [Angular] Create Category

--- FrontEnd/WebVersion/src/app/app-routing.module.ts
+++ FrontEnd/WebVersion/src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { CategroyComponent } from './admin/pages/categroy/categroy.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { CreateCategoryComponent } from './admin/pages/Categroy/create-category/create-category.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DefaultLayoutComponent,
     children:[
+    ],
+  },
+  {
+    path: 'admin',
+    component: DefaultLayoutComponent,
+    children:[
+      { path: 'categories/create', component: CreateCategoryComponent},
       { path: 'categories', component: CategroyComponent},
     ],
   }
+
 ];
 
 @NgModule({
---
+++ FrontEnd/WebVersion/src/app/services/category.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from '../models/category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http:HttpClient) { }
+
+  getAll(){
+    return this.http.get<Category[]>("http://34.199.239.78:8888/CATEGORY-SERVICE/categories");
+  }
+
+  createCategory(formData:FormData){
+      return this.http.post("http://34.199.239.78:8888/CATEGORY-SERVICE/categories",formData);
+  }
+}
