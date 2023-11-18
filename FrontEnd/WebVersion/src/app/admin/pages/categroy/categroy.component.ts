4eadbd02dc57951ae2957dcc35d05ea056a37b3e
Haythem Dridi <haithemdridiweb@gmail.com>
2023-11-18 16:30:21 +0100

    [Angular] Get categories

--- FrontEnd/WebVersion/src/app/admin/pages/categroy/categroy.component.ts
+++ FrontEnd/WebVersion/src/app/admin/pages/categroy/categroy.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-categroy',
   templateUrl: './categroy.component.html',
   styleUrls: ['./categroy.component.css']
 })
-export class CategroyComponent {
+export class CategroyComponent implements OnInit {
+
+  categories : Category[] = [];
+
+  constructor(private service:CategoryService){
+
+  }
+
+
+  ngOnInit() {
+    this.service.getAll().subscribe(
+      (response) => {
+        this.categories = response;
+      }
+    );
+  }
 
 }
