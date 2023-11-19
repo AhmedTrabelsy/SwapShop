bb31a79f032947364d4c2b6fff4c9c693bbda0fc
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-19 11:00:29 +0100

    create the nav component

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop-client/src/app/shared/nav/nav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swap-shop-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+export class NavComponent {
+
+}
