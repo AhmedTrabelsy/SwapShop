a5458add9f6e5c296d10584967560a871fb3c71d
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-19 10:17:49 +0100

    import the angular animations module

--- FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop-client/src/app/app.module.ts
+++ FrontEnd/WebVersion/SwapShop/swap-shop/apps/swap-shop-client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { AccordionModule } from 'primeng/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, ProductListComponent, FooterComponent, HeaderComponent],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), AccordionModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
   exports: [
