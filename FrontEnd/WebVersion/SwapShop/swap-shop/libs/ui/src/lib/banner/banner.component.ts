fb508d22bcb5db8c4c4675bed3319ef884e8b4f9
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-19 05:51:54 +0100

    generate the ui/banner component

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/libs/ui/src/lib/banner/banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-banner',
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.scss'
+})
+export class BannerComponent {
+
+}
--- FrontEnd/WebVersion/SwapShop/swap-shop/libs/ui/src/lib/ui.module.ts
+++ FrontEnd/WebVersion/SwapShop/swap-shop/libs/ui/src/lib/ui.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BannerComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BannerComponent
   ]
 })
 export class UiModule { }
