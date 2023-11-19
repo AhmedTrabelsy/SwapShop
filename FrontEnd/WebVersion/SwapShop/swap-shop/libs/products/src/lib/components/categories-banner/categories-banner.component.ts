49f50e7500360c02f958ee1a0c96cc9f90ae6ddd
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-19 17:47:35 +0100

    create the categories banner component

---
+++ FrontEnd/WebVersion/SwapShop/swap-shop/libs/products/src/lib/components/categories-banner/categories-banner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'swap-shop-categories-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './categories-banner.component.html',
+  styles: ``
+})
+export class CategoriesBannerComponent {
+
+}
