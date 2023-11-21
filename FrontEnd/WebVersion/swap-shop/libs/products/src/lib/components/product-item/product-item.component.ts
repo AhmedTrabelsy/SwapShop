7cf6a9f55a18e8eebbae6f6bfbe1359d221c9487
farouk <chessplayer869@gmail.com>
2023-11-21 15:50:02 +0100

    implement product item component

--- FrontEnd/WebVersion/swap-shop/libs/products/src/lib/components/product-item/product-item.component.ts
+++ FrontEnd/WebVersion/swap-shop/libs/products/src/lib/components/product-item/product-item.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { product } from '../../models/product';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'swap-shop-product-item',
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
-export class ProductItemComponent {
+export class ProductItemComponent implements OnInit, OnDestroy {
 
+   products: product[] = [];
+   private productSubscription?: Subscription;
+   constructor(private productService: ProductService) { }
+
+   ngOnInit(): void {
+     this.productSubscription = this.productService
+       .getProducts().subscribe((products) => {
+         this.products = products;
+       });
+   }
+
+   ngOnDestroy(): void {
+     // Unsubscribe from the subscription to avoid memory leaks
+     if (this.productSubscription) {
+       this.productSubscription.unsubscribe();
+     }
+   }
 }
+
+
+
+
+
