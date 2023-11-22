3c45c1d53b396a8bfbad44c595309d217ee1425e
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-22 08:00:54 +0100

    add timeago and format currency functions to product-item

--- FrontEnd/WebVersion/swap-shop/libs/products/src/lib/components/product-item/product-item.component.ts
+++ FrontEnd/WebVersion/swap-shop/libs/products/src/lib/components/product-item/product-item.component.ts
@@ -1,14 +1,32 @@
 import { product } from '../../models/product';
 import { Component, Input } from '@angular/core';
+import { formatDistanceToNow } from 'date-fns';
 
 @Component({
   selector: 'swap-shop-product-item',
   templateUrl: './product-item.component.html',
 })
-export class ProductItemComponent{
+export class ProductItemComponent {
+  @Input() product?: product
 
-   @Input () product?: product
+  getTimeAgo(creationDate?: Date): string {
+    if (creationDate) {
+      return formatDistanceToNow(new Date(creationDate), { addSuffix: true });
+    }
+    return '';
+  }
 
+  formatCurrency(price?: number): string {
+    if (price) {
+      if ((price | 0) < price) {
+        const formattedPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'TND' }).format(price);
+        return formattedPrice.replace(',', ' ');
+      }
+      const formattedPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'TND', maximumFractionDigits: 0 }).format(price);
+      return formattedPrice.replace(',', ' ');
+    }
+    return 'Undefined';
+  }
 }
 
 
