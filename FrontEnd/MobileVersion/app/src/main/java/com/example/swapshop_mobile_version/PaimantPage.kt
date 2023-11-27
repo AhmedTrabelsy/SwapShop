b85cbab723572743f77c6ec7564faad253e333a7
hakimBarbaria <hakimBarbaria589@gmail.com>
2023-11-26 21:16:01 -0800

    correct the checkout successfully

--- FrontEnd/MobileVersion/app/src/main/java/com/example/swapshop_mobile_version/PaimantPage.kt
+++ FrontEnd/MobileVersion/app/src/main/java/com/example/swapshop_mobile_version/PaimantPage.kt
@@ -14,26 +14,28 @@ class PaimantPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paimant_page)
-        getSupportActionBar()!!.setTitle("Checkout")
+        getSupportActionBar()?.setTitle("Checkout")
+
         val paimantButton = findViewById<Button>(R.id.Billing)
         val confirm = findViewById<CheckBox>(R.id.checkBox)
-        val email = findViewById<EditText>(R.id.email).text.toString()
-        val phone = findViewById<EditText>(R.id.numberPhone).text.toString()
-        val numberCard = findViewById<EditText>(R.id.numberCard).text.toString()
-        val billingAdress = findViewById<EditText>(R.id.billingAdress).text.toString()
-            val extras = intent.extras
-            val price = extras?.getString("price")
-            paimantButton.text = "Pay $price DT"
+        val extras = intent.extras
+        val price = extras?.getString("price")
+        paimantButton.text = "Pay $price DT"
 
-            paimantButton.setOnClickListener {
-                if (email.isEmpty() || phone.isEmpty() || numberCard.isEmpty() || billingAdress.isEmpty()) {
-                    val snackbar = Snackbar.make(
-                        findViewById<View>(android.R.id.content),
-                        "Error! Please confirm your data",
-                        Snackbar.LENGTH_LONG
-                    )
-                    snackbar.show()
-                } else {
+        paimantButton.setOnClickListener {
+            val email = findViewById<EditText>(R.id.email).text.toString()
+            val phone = findViewById<EditText>(R.id.numberPhone).text.toString()
+            val numberCard = findViewById<EditText>(R.id.numberCard).text.toString()
+            val billingAdress = findViewById<EditText>(R.id.billingAdress).text.toString()
+
+            if (email.isEmpty() || phone.isEmpty() || numberCard.isEmpty() || billingAdress.isEmpty()) {
+                val snackbar = Snackbar.make(
+                    findViewById<View>(android.R.id.content),
+                    "Error! Please confirm your data",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.show()
+            } else {
                 if (confirm.isChecked) {
                     Toast.makeText(this, "Checkout successful", Toast.LENGTH_LONG).show()
                     finish()
