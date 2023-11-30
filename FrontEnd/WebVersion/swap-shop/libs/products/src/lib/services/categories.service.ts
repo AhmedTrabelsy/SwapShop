dcab9ebcb3fa1dae21306f6b72d61aff52dc9ce7
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-30 11:25:13 +0100

    replace form data in the categories service

--- FrontEnd/WebVersion/swap-shop/libs/products/src/lib/services/categories.service.ts
+++ FrontEnd/WebVersion/swap-shop/libs/products/src/lib/services/categories.service.ts
@@ -17,12 +17,12 @@ export class CategoriesService {
     return this.http.get<Category>("http://34.199.239.78:8888/CATEGORY-SERVICE/categories/" + id);
   }
 
-  createCategory(formData: FormData) {
-    return this.http.post("http://34.199.239.78:8888/CATEGORY-SERVICE/categories", formData);
+  createCategory(category: Category) {
+    return this.http.post("http://34.199.239.78:8888/CATEGORY-SERVICE/categories", category);
   }
 
-  editCategoryById(id: number, formData: FormData) {
-    return this.http.put("http://34.199.239.78:8888/CATEGORY-SERVICE/categories/" + id, formData);
+  editCategoryById(id: number, category: Category) {
+    return this.http.put("http://34.199.239.78:8888/CATEGORY-SERVICE/categories/" + id, category);
   }
 
   deleteCategoryById(id: number) {
