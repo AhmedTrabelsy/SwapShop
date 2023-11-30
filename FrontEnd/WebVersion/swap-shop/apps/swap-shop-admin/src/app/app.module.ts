e43ef16d7e02da4da777cac0ead7bf8fbc856693
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-11-30 08:10:23 +0100

    create the admin products list and form

--- FrontEnd/WebVersion/swap-shop/apps/swap-shop-admin/src/app/app.module.ts
+++ FrontEnd/WebVersion/swap-shop/apps/swap-shop-admin/src/app/app.module.ts
@@ -7,16 +7,20 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
 
 @NgModule({
-	declarations: [AppComponent, NxWelcomeComponent, SidebarComponent, ShellComponent, DashboardComponent],
+	declarations: [AppComponent, NxWelcomeComponent, SidebarComponent, ShellComponent, DashboardComponent, ProductsFormComponent, ProductsListComponent],
 	imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
 	providers: [],
 	bootstrap: [AppComponent],
 	exports: [
    SidebarComponent,
    ShellComponent,
-   DashboardComponent
+   DashboardComponent,
+   ProductsFormComponent,
+   ProductsListComponent
 	]
 })
 export class AppModule {}
---
+++ FrontEnd/WebVersion/swap-shop/apps/swap-shop-admin/src/app/pages/products/products-list/products-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'swap-shop-products-list',
+  templateUrl: './products-list.component.html',
+  styles: ``
+})
+export class ProductsListComponent {
+
+}
