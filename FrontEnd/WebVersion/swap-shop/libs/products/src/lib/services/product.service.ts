09befa4c870ffe14c124073b877a5769bc60f257
Ahmed Trabelsi <Trabelsig37@gmail.com>
2023-12-01 08:41:39 +0100

    implements categories list

--- FrontEnd/WebVersion/swap-shop/libs/products/src/lib/services/product.service.ts
+++ FrontEnd/WebVersion/swap-shop/libs/products/src/lib/services/product.service.ts
@@ -11,7 +11,23 @@ export class ProductService {
 
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<product[]> {
-    return this.http.get<product[]>("http://34.199.239.78:8888/PRODUCT-SERVICE/products");
+  getProducts(): Observable<never> {
+    return this.http.get<never>("http://34.199.239.78:8888/PRODUCT-SERVICE/products");
+  }
+
+  createProduct(productData: FormData): Observable<product> {
+    return this.http.post<product>("http://34.199.239.78:8888/PRODUCT-SERVICE/products", productData);
+  }
+
+  getProduct(productId: string): Observable<product> {
+    return this.http.get<product>(`${"http://34.199.239.78:8888/PRODUCT-SERVICE/products"}/${productId}`);
+  }
+
+  updateProduct(productData: FormData, productid: string): Observable<product> {
+    return this.http.put<product>(`${"http://34.199.239.78:8888/PRODUCT-SERVICE/products"}/${productid}`, productData);
+  }
+
+  deleteProduct(productId: string): Observable<unknown> {
+    return this.http.delete<unknown>(`${"http://34.199.239.78:8888/PRODUCT-SERVICE/products"}/${productId}`);
   }
 }
