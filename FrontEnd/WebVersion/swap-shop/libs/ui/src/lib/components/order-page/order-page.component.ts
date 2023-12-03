25bb10a7ff2344272debfdc60416f5e1b7e64861
beyremagrebi <beyremagrebi58@gmail.com>
2023-12-03 14:46:47 +0100

    ready waiting table order

--- FrontEnd/WebVersion/swap-shop/libs/ui/src/lib/components/order-page/order-page.component.ts
+++ FrontEnd/WebVersion/swap-shop/libs/ui/src/lib/components/order-page/order-page.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,Injectable } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FieldsetModule } from 'primeng/fieldset';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
 	selector: 'swap-shop-order-page',
 	standalone: true,
 	imports: [
 		CommonModule,
 		FieldsetModule,
+		HttpClientModule
 	],
 	templateUrl: './order-page.component.html',
 	styleUrls: ['./order-page.component.css']
 })
-export class OrderPageComponent {
+@Injectable({
+	providedIn: 'root'
+  })
+export class OrderPageComponent implements OnInit{
+	private apiUrl = 'http://34.199.239.78:8888/PRODUCT-SERVICE/products/21';
+	public data:any={
+
+	};
+	constructor(private http: HttpClient) { }
+	ngOnInit(): void {
+		// Call the method to fetch data when the component is initialized
+	this.http.get<any>(this.apiUrl).subscribe((res) => {
+        this.data = res;
+        console.log(this.data);
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+	
+	  }
+
 	formatCurrency(price?: number): string {
 		if (price) {
 		  if ((price | 0) < price) {
