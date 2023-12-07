701173bb8ee93b23435699bf240be683182496ac
Haythem Dridi <haithemdridiweb@gmail.com>
2023-12-07 21:32:30 +0100

    [Android] Save Settings]

--- FrontEnd/MobileVersion/app/src/main/java/com/example/swapshop_mobile_version/GetUserData.kt
+++ FrontEnd/MobileVersion/app/src/main/java/com/example/swapshop_mobile_version/GetUserData.kt
@@ -2,10 +2,10 @@ package com.example.swapshop_mobile_version
 
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 data class LoginData(
     val username: String,
@@ -21,6 +21,14 @@ data class SignupData(
     val password: String
 )
 
+data class updateProfileDate(
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val phoneNumber: String,
+)
+
+
 interface GetUserData {
     @POST("AUTH-SERVICE/login")
     fun getAccessToken(@Body loginData: LoginData): Call<AccessToken>
@@ -31,5 +39,8 @@ interface GetUserData {
 
     @GET("AUTH-SERVICE/user")
     fun getData(@Header("Authorization") token: String): Call<getUserResponse>
+
+    @PUT("AUTH-SERVICE/update")
+    fun updateProfile(@Header("Authorization") token: String, @Body updateProfileDate:updateProfileDate): Call<getUserResponse>
 
 }
