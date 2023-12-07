dc07fa639cd5c0d9de66042cebcf90364b40c9b6
Haythem Dridi <haithemdridiweb@gmail.com>
2023-12-07 19:17:35 +0100

    [NodeJs] Improve get user Data

--- BackEnd/NodeJs/auth-service/controllers/userController.js
+++ BackEnd/NodeJs/auth-service/controllers/userController.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { getAdminToken } = require('./authController');
 
 exports.getUserData = async (req, res) => {
 	const token = req.headers.authorization;
@@ -16,5 +17,15 @@ exports.getUserData = async (req, res) => {
 		},
 	});
 
-	return res.status(200).json(response.data);
+	let username = response.data['preferred_username'];
+
+	const adminToken = await getAdminToken();
+
+	const response2 = await axios.get('http://34.199.239.78:8080/admin/realms/SwapShop/users?username=' + username, {
+		headers: {
+			Authorization: `Bearer ${adminToken}`,
+		},
+	});
+
+	return res.status(200).json(response2.data[0]);
 };
