7b7ab2b478f35a2c2c5656f3bd1b4ab13d02888b
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2023-12-08 09:10:41 +0100

    ceperate app and server .ts files

--- BackEnd/NodeJs/news-service/app.ts
+++ BackEnd/NodeJs/news-service/app.ts
@@ -2,7 +2,7 @@ import express, { ErrorRequestHandler } from 'express';
 import multer from 'multer';
 import { handleFileUpload, retrieveUploadedFile } from './controllers/news-controller';
 require('express-async-errors');
-const app = express();
+export const app = express();
 
 app.use(express.json({ limit: '10kb' }));
 
@@ -22,12 +22,3 @@ app.use(errorHandler);
 
 app.post('/upload', upload.single('image'), handleFileUpload);
 app.get('/uploads/:filename', retrieveUploadedFile);
-
-
-// Start the server
-const port: number = 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-
-module.exports = app;
---
+++ BackEnd/NodeJs/news-service/server.ts
@@ -0,0 +1,12 @@
+const app = require('./app');
+
+const EurekaClient = require('./eureka');
+EurekaClient.start();
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
