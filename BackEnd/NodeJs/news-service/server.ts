4b0fc2cb42dd8fc052e1b1ecc2c5306576149331
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2023-12-08 14:01:22 +0100

    connect to mongoDb database

--- BackEnd/NodeJs/news-service/server.ts
+++ BackEnd/NodeJs/news-service/server.ts
@@ -1,11 +1,23 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const EurekaClient = require('./eureka');
 EurekaClient.start();
 
-// Start the server
 const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE;
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log('DB connection successful');
+    });
+
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
