8b139cea5a2166761188d12e0fbd9aebff3955ec
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2023-12-08 05:23:27 +0100

    add error logger

--- BackEnd/NodeJs/news-service/index.ts
+++ BackEnd/NodeJs/news-service/index.ts
@@ -1,5 +1,20 @@
-import express from "express";
-
+import express, { ErrorRequestHandler } from 'express';
+// const { create, get, deleteByUserId, deleteProductFromUserWishList } = require('./controllers/wishListController');
 const app = express();
+require('express-async-errors');
 
-app.listen(8000, () => console.log("listening on port 8000..."));
+app.use(express.json({ limit: '10kb' }));
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+};
+app.use(errorHandler);
+
+
+// app.get('/:user_id', get);
+// app.delete('/:user_id', deleteByUserId);
+// app.delete('/:user_id/:product_id', deleteProductFromUserWishList);
+// app.post('/', create);
+
+module.exports = app;
