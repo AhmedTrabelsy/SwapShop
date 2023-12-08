53cb7ae02050bd587ab56b9a8493ead3b41e0c29
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2023-12-08 09:10:59 +0100

    add eureka client file

---
+++ BackEnd/NodeJs/news-service/eureka.ts
@@ -0,0 +1,55 @@
+const Eureka = require('eureka-js-client').Eureka;
+
+const createEurekaClient = () => {
+	return new Eureka({
+		instance: {
+			app: 'NEWS-SERVICE',
+			hostName: 'news',
+			ipAddr: '127.0.0.1',
+			vipAddress: 'NEWS-SERVICE',
+			port: {
+				$: 8084,
+				'@enabled': 'true',
+			},
+			dataCenterInfo: {
+				'@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+				name: 'MyOwn',
+			},
+		},
+		eureka: {
+			host: process.env.EUREKA_HOST || 'localhost',
+			port: 8761,
+		},
+	});
+};
+
+const connectToEureka = (eurekaClient: typeof Eureka, maxRetries: number, retryInterval: number) => {
+	let retries = 0;
+
+	const tryConnect = () => {
+		eurekaClient.start((error: Error) => {
+			if (error) {
+				console.error(`Failed to connect to Eureka server. Retrying in ${retryInterval / 1000} seconds...`);
+				retries++;
+
+				if (retries < maxRetries || maxRetries === -1) {
+					setTimeout(tryConnect, retryInterval);
+				} else {
+					console.error(`Max retries reached. Unable to connect to Eureka server.`);
+				}
+			} else {
+				console.log('Connected to Eureka server.');
+			}
+		});
+	};
+
+	tryConnect();
+};
+
+const maxRetries = -1;
+const retryInterval = 5000;
+
+const eurekaClient = createEurekaClient();
+connectToEureka(eurekaClient, maxRetries, retryInterval);
+
+module.exports = eurekaClient;
