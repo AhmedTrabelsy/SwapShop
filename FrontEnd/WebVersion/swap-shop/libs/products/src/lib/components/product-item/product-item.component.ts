30fb585e0e6e89996b951cb3f6a1654c78093462
hakimBarbaria <hakimBarbaria589@gmail.com>
2023-12-18 11:39:40 -0800

    resolving the merge conflicts in client nx app

--- FrontEnd/WebVersion/swap-shop/libs/products/src/lib/components/product-item/product-item.component.ts
+++ FrontEnd/WebVersion/swap-shop/libs/products/src/lib/components/product-item/product-item.component.ts
@@ -1,12 +1,7 @@
 import { product } from '../../models/product';
 import { Component, Input } from '@angular/core';
 import { formatDistanceToNow } from 'date-fns';
-<<<<<<< HEAD
-import { BadgeModule } from 'primeng/badge';
-=======
 import { WishlistService } from '../../services/wishlist.service';
->>>>>>> 4bd670574288a8f7ab9abbcc04fdd3823939e7bc
-
 @Component({
   selector: 'swap-shop-product-item',
   templateUrl: './product-item.component.html',
@@ -15,19 +10,14 @@ export class ProductItemComponent{
   @Input() product?: product
   wishlistProducts?: product[];
 
-<<<<<<< HEAD
   badgeStatus: 'success' | 'info' | 'warning' | 'danger' | null | undefined ='danger'
   badgeValue="Unsigned"
   sellerName="name"
-=======
 
   constructor(private wishlistService: WishlistService) { }
 
   heartClass: string = 'pi pi-heart';
   wishlistText: string = ' Add to Wishlist';
-
-
-
 
   toggleHeart(idString: string|undefined): void {
     const id = parseInt(idString || '0');
@@ -41,7 +31,6 @@ export class ProductItemComponent{
       this.wishlistText = ' Add to Wishlist';
     }
   }
->>>>>>> 4bd670574288a8f7ab9abbcc04fdd3823939e7bc
   getTimeAgo(creationDate?: Date): string {
     if (creationDate) {
       return formatDistanceToNow(new Date(creationDate), { addSuffix: true });
