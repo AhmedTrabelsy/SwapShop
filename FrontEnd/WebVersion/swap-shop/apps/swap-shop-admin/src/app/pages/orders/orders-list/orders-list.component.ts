438859ff94de907725b285db9ae3fdd968e2f507
Ahmed Trabelsi <92649043+AhmedTrabelsy@users.noreply.github.com>
2024-01-08 10:34:07 +0100

    list orders admin panel

--- FrontEnd/WebVersion/swap-shop/apps/swap-shop-admin/src/app/pages/orders/orders-list/orders-list.component.ts
+++ FrontEnd/WebVersion/swap-shop/apps/swap-shop-admin/src/app/pages/orders/orders-list/orders-list.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from 'libs/products/src/lib/services/order.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'swap-shop-orders-list',
   templateUrl: './orders-list.component.html',
   styles: ``
 })
-export class OrdersListComponent {
+export class OrdersListComponent implements OnInit, OnDestroy {
+  orders = [];
+  endsubs$: Subject<unknown> = new Subject();
 
+  constructor(
+    private orderService: OrderService,
+    private router: Router,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService
+  ) { }
+
+  ngOnInit(): void {
+    this._getOrders();
+  }
+
+  ngOnDestroy() {
+    this.endsubs$.complete();
+  }
+
+  private _getOrders() {
+    this.orderService
+      .getOrders()
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe((orders) => {
+        this.orders = orders;
+      });
+  }
+
+  deleteOrder(orderId: string) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this Order?',
+      header: 'Delete Order',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.orderService
+          .deleteOrder(orderId)
+          .pipe(takeUntil(this.endsubs$))
+          .subscribe(
+            () => {
+              this._getOrders();
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Success',
+                detail: 'Order is deleted!'
+              });
+            },
+            () => {
+
+              this._getOrders();
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Success',
+                detail: 'Order is deleted!'
+              });
+            }
+          );
+      }
+    });
+  }
 }
--- FrontEnd/WebVersion/swap-shop/libs/products/src/lib/services/order.service.ts
+++ FrontEnd/WebVersion/swap-shop/libs/products/src/lib/services/order.service.ts
@@ -8,14 +8,20 @@ import { HttpClient } from '@angular/common/http';
 export class OrderService {
   BASE_URL_ORDER_SERVICE = "http://34.199.239.78:8888/ORDER-SERVICE/"
   constructor(private http: HttpClient) {
-   }
-   getOrderCount(): Observable<number> {
-    return this.http.get<number>(this.BASE_URL_ORDER_SERVICE+"orderCount");
+  }
+  getOrders(): Observable<never[]> {
+    return this.http.get<never>(this.BASE_URL_ORDER_SERVICE + "orders");
+  }
+  getOrderCount(): Observable<number> {
+    return this.http.get<number>(this.BASE_URL_ORDER_SERVICE + "orderCount");
   }
   getOrdersPricePerMounth(): Observable<never[]> {
-    return this.http.get<never[]>(this.BASE_URL_ORDER_SERVICE+"orderPerMonth");
+    return this.http.get<never[]>(this.BASE_URL_ORDER_SERVICE + "orderPerMonth");
   }
   getLastUpdatedOrders(): Observable<never[]> {
-    return this.http.get<never>(this.BASE_URL_ORDER_SERVICE+"getLastOrders");
+    return this.http.get<never>(this.BASE_URL_ORDER_SERVICE + "getLastOrders");
+  }
+  deleteOrder(orderId: string): Observable<unknown> {
+    return this.http.delete<unknown>(this.BASE_URL_ORDER_SERVICE + "orders/" + orderId);
   }
 }
