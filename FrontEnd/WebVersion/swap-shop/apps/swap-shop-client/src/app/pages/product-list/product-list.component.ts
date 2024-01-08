875e6099342f474e08ab9ddf4327a43d8571588b
hakimBarbaria <hakimBarbaria589@gmail.com>
2024-01-07 16:19:22 -0800

    implement the add to wishlist functionnality

--- FrontEnd/WebVersion/swap-shop/apps/swap-shop-client/src/app/pages/product-list/product-list.component.ts
+++ FrontEnd/WebVersion/swap-shop/apps/swap-shop-client/src/app/pages/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { product } from 'libs/products/src/lib/models/product';
 import { ProductService } from 'libs/products/src/lib/services/product.service';
+import { WishlistService } from 'libs/products/src/lib/services/wishlist.service';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,13 +12,46 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   products: product[] = [];
   private productSubscription?: Subscription;
-  constructor(private productService: ProductService) { }
-  
+  constructor(private productService: ProductService,private wishlistService: WishlistService) { 
+  }
+  icons: string[] = [];
+  onClick(event: MouseEvent, index: number, idString: string|undefined) {
+    const id = parseInt(idString || '0');
+    event.preventDefault();
+    if (this.icons[index] === "pi pi-heart-fill") {
+      this.deleteFromWishlist(idString);
+      this.icons[index] = "pi pi-heart";
+    }else {
+      this.onAddToWishlist(id);
+      this.icons[index] = "pi pi-heart-fill";
+    }
+  }
   ngOnInit(): void {
     this.productSubscription = this.productService
       .getProducts().subscribe((products) => {
         this.products = products;
+        this.icons = new Array(this.products.length).fill("pi pi-heart");
+        console.log(this.icons);
       });
+  }
+  onAddToWishlist(productId: number): void {
+    this.wishlistService.addToWishlist(productId)
+      .subscribe(
+        (response) => {
+          console.log('Added to wishlist:', response);
+        },
+        (error) => {
+          console.error('Error adding to wishlist:', error);
+        }
+      );
+  }
+  deleteFromWishlist(idString?: string | undefined): void {
+    if (idString !== undefined) {
+      const id = parseInt(idString, 10);
+      this.wishlistService.deleteProduct(id);
+    } else {
+      console.error('Item ID is undefined.');
+    }
   }
   ngOnDestroy(): void {
     if (this.productSubscription) {
