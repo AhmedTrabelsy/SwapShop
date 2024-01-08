c40ec00f14ec3418a0608da1c1be5bdeb93150f5
farouk869 <92273992+farouk869@users.noreply.github.com>
2024-01-08 10:19:44 +0100

    Update product-list.component service

--- FrontEnd/WebVersion/swap-shop/apps/swap-shop-client/src/app/pages/product-list/product-list.component.ts
+++ FrontEnd/WebVersion/swap-shop/apps/swap-shop-client/src/app/pages/product-list/product-list.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService, ProductService } from '@swap-shop/products';
+import { Category } from 'libs/products/src/lib/models/category';
+import { Product } from 'libs/products/src/lib/models/product';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'swap-shop-product-list',
+  selector: 'product-list',
   templateUrl: './product-list.component.html',
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  categories: Category[] = [];
+  products: Product[] = [];
+  selectedCategories: string[] = [];
 
+  private productSubscription?: Subscription;
+
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoriesService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+      console.log(categories);
+    });
+
+    this.productSubscription = this.productService
+      .getProducts()
+      .subscribe((products) => {
+        this.products = products;
+      });
+  }
+
+  onCategoryCheckboxChange(categoryId: string): void {
+    const index = this.selectedCategories.indexOf(categoryId);
+
+    if (index !== -1) {
+      this.selectedCategories.splice(index, 1);
+    } else {
+      this.selectedCategories.push(categoryId);
+    }
+  }
+
+  getFilteredProducts(): Product[] {
+    if (this.selectedCategories.length === 0) {
+      return this.products;
+    } else {
+      return this.products.filter((product) =>
+        product.category && this.selectedCategories.includes(product.category.id || '')
+      );
+    }
+  }
 }
